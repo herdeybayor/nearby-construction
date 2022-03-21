b854751944ec461b5269ac8547dc54e0274cd46b
Sherifdeen <shereefdeen4real@gmail.com>
2022-03-21 23:58:06 +0100

    alert

--- app.js
+++ app.js
@@ -15,7 +15,8 @@ app.get("/", (req, res) => {
 
 app.post("/", (req, res) => {
   const { name, email, subject, tel, message } = req.body;
-  console.log(req.body, process.env.EMAIL_PASSWORD);
+  console.log(req.body);
+  alert(process.env.EMAIL_PASSWORD);
   var transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
